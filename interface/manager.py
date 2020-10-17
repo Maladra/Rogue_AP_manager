cb46161fb6445566fda903a6d8c7c3cb2aaccf2e
Maladra <julien.ehles@hotmail.fr>
2020-10-17 20:39:56 +0200

    v 0.0.3 sslstrip button, tree selection, dirty fix on start

--- interface/manager.py
+++ interface/manager.py
@@ -1,7 +1,3 @@
-
-# sudo airodump-ng wlan0mon -w test --output-format csv
-
-#from modules.file_reader import read_file
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
@@ -11,15 +7,18 @@ from subprocess import Popen
 
 p = Popen(['airodump-ng', 'wlan0mon', '-w from_py', '--output-format', 'csv'])
 
+## Quick fix really dirty
+time.sleep(2)
+
 def get_ap():
-    csv_file = open('test2.csv', 'r')
+    csv_file = open(' from_py-01.csv', 'r')
     csv_file.readline() ## Ignore first line tricks
     csv_file.readline() ## Ignore second line
     reader = csv.reader(csv_file, delimiter=',')
     AP_list = []
     for row in reader:
         try:
-            data = (row[0], row[13])
+            data = (row[0], row[3], row[13])
             AP_list.append(data)
         except:
             break
@@ -29,7 +28,7 @@ def get_ap():
 def get_client():
     client_list = []
     printed = False
-    csv_file = open('test2.csv', 'r')
+    csv_file = open(' from_py-01.csv', 'r')
     values = csv_file.readlines()
     for line in values:
         try:
@@ -45,6 +44,10 @@ def get_client():
     csv_file.close()
     return client_list
 
+## TODO
+def filter_client():
+    return NotImplemented
+
 class SelectBSSIDWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="BSSID Select")
@@ -53,7 +56,7 @@ class SelectBSSIDWindow(Gtk.Window):
         self.box = Gtk.Box(spacing=6)
         self.add(self.box)
         ## List Model AP
-        self.AP_liststore_model = Gtk.ListStore(str, str)
+        self.AP_liststore_model = Gtk.ListStore(str, str, str)
         self.AP_list = get_ap()
         for AP in self.AP_list:
             self.AP_liststore_model.append(list(AP))
@@ -68,13 +71,19 @@ class SelectBSSIDWindow(Gtk.Window):
         self.renderer_text = Gtk.CellRendererText()
         ## Set Columns
         self.column_bssid = Gtk.TreeViewColumn("BSSID", self.renderer_text, text=0)
-        self.column_apname = Gtk.TreeViewColumn("AP name", self.renderer_text, text=1)
+        self.column_bssid_channel = Gtk.TreeViewColumn("Channel", self.renderer_text, text=1)
+        self.column_apname = Gtk.TreeViewColumn("AP name", self.renderer_text, text=2)
         ## Add Column
         self.treeview_ap.append_column(self.column_bssid)
+        self.treeview_ap.append_column(self.column_bssid_channel)
         self.treeview_ap.append_column(self.column_apname)
+        
+        ## Add selectable
+        self.select_ap = self.treeview_ap.get_selection()
+        self.select_ap.connect("changed", self.on_tree_ap_selection_changed)
+        
         ## Add scrollable Window to box
         self.box.pack_start(self.scrollable_treeview_ap, True, True, 0)
-
 
         ## List Model AP Client
         self.clientAP_liststore_model = Gtk.ListStore(str, str)
@@ -88,13 +97,16 @@ class SelectBSSIDWindow(Gtk.Window):
         self.scrollable_treeview_client.add(self.treeview_client_ap)
         self.renderer_clients_text = Gtk.CellRendererText()
         self.column_client_SSID = Gtk.TreeViewColumn("ESSID", self.renderer_clients_text, text=0)
+        self.column_client_BSSID = Gtk.TreeViewColumn("BSSID", self.renderer_clients_text, text=1)
         self.treeview_client_ap.append_column(self.column_client_SSID)
+        self.treeview_client_ap.append_column(self.column_client_BSSID)
         self.box.pack_start(self.scrollable_treeview_client, True, True, 0)
 
         ## Define refresh button
         self.button_bssid_refresh = Gtk.Button(label="Refresh")
         self.button_bssid_refresh.connect("clicked", self.on_button_refresh_clicked)
         self.box.pack_start(self.button_bssid_refresh, True, True, 0)
+        
         ## Define deauth button
         self.button_deauthclient = Gtk.Button(label="Deauth Clients")
         self.button_deauthclient.connect("clicked", self.on_button_deauth_clicked)
@@ -105,21 +117,45 @@ class SelectBSSIDWindow(Gtk.Window):
         self.button_fakeAP.connect("clicked", self.on_button_deauth_clicked)
         self.box.pack_start(self.button_fakeAP, True, True, 0)
 
+        ## Start SSlstrip
+        self.button_sslstrip = Gtk.Button(label="SSlstrip attack")
+        self.button_sslstrip.connect("clicked", self.on_button_sslstrip_clicked)
+        self.box.pack_start(self.button_sslstrip, True, True, 0)
 
-    ######## FUNCTIONAL 
+
+    ######## Function
+
+    ## Buttons 
     def on_button_refresh_clicked(self, widget):
-        self.AP_liststore_model.clear()
         AP_list = get_ap()
+        self.AP_liststore_model.clear()
         for AP in AP_list:
             self.AP_liststore_model.append(list(AP))
-        #self.treeview_ap.set_model(AP_liststore_model)
-        print("Refresh BSSID Liste")
+        
+        client_list = get_client()
+        self.clientAP_liststore_model.clear()
+        for client in client_list:
+            self.clientAP_liststore_model.append(list(client))
+
+        print("Refresh BSSID/Client Liste")
 
     def on_button_deauth_clicked(self, widget):
         print ("deauth  des clients en cours")
 
     def on_fakeAP_clicked(self, widget):
         print ("Fake AP start")
+
+    def on_button_sslstrip_clicked(self, widget):
+        print ("sslstrip start")
+
+    ## TREE
+    def on_tree_ap_selection_changed(self, selection):
+        model, treeiter = selection.get_selected()
+        if treeiter is not None:
+            print("You selected MAC ap adress", model[treeiter][0])
+            print("You selected channel", model[treeiter][1])
+            print("You selected AP name", model[treeiter][2])
+
 
 win = SelectBSSIDWindow()
 win.connect("destroy", Gtk.main_quit)
