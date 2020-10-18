087d0f4f54045dbfa5eeb7a10a482c652b15a509
Maladra <julien.ehles@hotmail.fr>
2020-10-18 14:49:03 +0200

    script to config monitor mode

--- aircrack_command.sh
+++
@@ -1,2 +0,0 @@
-sudo airmon-ng start wlx78d294c227c2 ##modifier en fonction de l'interface
-sudo airmon-ng check kill
---
+++ monitor_mode.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "---------------------------------"
+iw dev | awk '$1=="Interface"{print $2}'
+echo "---------------------------------"
+
+echo "Select interface to start Airodump :"
+read selected_interface
+
+sudo airmon-ng start $selected_interface
+sudo airmon-ng check kill
+echo ""
+#iw dev | awk '$1=="Interface"{print $2}'
+
